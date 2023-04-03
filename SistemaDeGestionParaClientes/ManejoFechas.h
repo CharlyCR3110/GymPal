e6fb108ab6b6da383d02e2979d7d9423dd695fa7
charly <carlosgarita3110@gmail.com>
2023-04-03 14:32:37 -0600

    feat: Add ManejoFechaClass with static methods

---
+++ SistemaDeGestionParaClientes/ManejoFechas.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Fecha.h"
+class ManejoFechas
+{
+public:
+	static int calcularEdad(Fecha* fechaNacimiento_, Fecha* fechaActual_);
+	static int calcularDiferencia(Fecha* fecha1_, Fecha* fecha2_);
+};
+
