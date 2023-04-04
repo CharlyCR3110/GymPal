344670fd386a7d230308342991e24baceb3e36a7
charly <carlosgarita3110@gmail.com>
2023-04-03 20:34:06 -0600

    feat: Add DatosBiometricos class: Add a new class called DatosBiometricos that represents a set of biometric data including height, weight, body fat percentage, muscle mass percentage, and the date the data was measured. The class also includes methods to set and get the data and a method to convert the data to a string. The class constructor checks if the input values are valid and throws a DatosBiometricosInvalidosException if they are not. The class also includes a destructor to free the memory allocated for the Fecha object.

---
+++ SistemaDeGestionParaClientes/DatosBiometricos.cpp
@@ -0,0 +1,101 @@
+#include "DatosBiometricos.h"
+
+DatosBiometricos::DatosBiometricos(float estatura_, float peso_, float porcentajeGrasaCorporal_, float porcentajeMasaMuscular_, Fecha* fechaMedicion_)
+{
+	if (estatura_ < 0)
+	{
+		throw DatosBiometricosInvalidosException("Estatura", "La estatura no puede ser negativa");
+	}
+
+	if (peso_ < 0)
+	{
+		throw DatosBiometricosInvalidosException("Peso", "El peso no puede ser negativo");
+	}
+
+	if (porcentajeGrasaCorporal_ < 0 || porcentajeGrasaCorporal_ > 100)
+	{
+		throw DatosBiometricosInvalidosException("Porcentaje de grasa corporal", "El porcentaje de grasa corporal debe estar entre 0 y 100");
+	}
+
+	if (porcentajeMasaMuscular_ < 0 || porcentajeMasaMuscular_ > 100)
+	{
+		throw DatosBiometricosInvalidosException("Porcentaje de masa muscular", "El porcentaje de masa muscular debe estar entre 0 y 100");
+	}
+
+	if (!fechaMedicion_->esValida(fechaMedicion_->getDia(), fechaMedicion_->getMes(), fechaMedicion_->getAnio()))
+	{
+		throw DatosBiometricosInvalidosException("Fecha de medicion", "La fecha de medicion no es valida");
+	}
+
+	estatura = estatura_;
+	peso = peso_;
+	porcentajeGrasaCorporal = porcentajeGrasaCorporal_;
+	porcentajeMasaMuscular = porcentajeMasaMuscular_;
+	fechaMedicion = fechaMedicion_;
+}
+
+DatosBiometricos::~DatosBiometricos()
+{
+}
+
+void DatosBiometricos::setEstatura(float estatura_)
+{
+	this->estatura = estatura_;
+}
+
+void DatosBiometricos::setPeso(float peso_)
+{
+	this->peso = peso_;
+}
+
+void DatosBiometricos::setPorcentajeGrasaCorporal(float porcentajeGrasaCorporal_)
+{
+	this->porcentajeGrasaCorporal = porcentajeGrasaCorporal_;
+}
+
+void DatosBiometricos::setPorcentajeMasaMuscular(float porcentajeMasaMuscular_)
+{
+	this->porcentajeMasaMuscular = porcentajeMasaMuscular_;
+}
+
+void DatosBiometricos::setFechaMedicion(Fecha* fechaMedicion_)
+{
+	this->fechaMedicion = fechaMedicion_;
+}
+
+float DatosBiometricos::getEstatura()
+{
+	return this->estatura;
+}
+
+float DatosBiometricos::getPeso()
+{
+	return this->peso;
+}
+
+float DatosBiometricos::getPorcentajeGrasaCorporal()
+{
+	return this->porcentajeGrasaCorporal;
+}
+
+float DatosBiometricos::getPorcentajeMasaMuscular()
+{
+	return this->porcentajeMasaMuscular;
+}
+
+Fecha* DatosBiometricos::getFechaMedicion()
+{
+	return this->fechaMedicion;
+}
+
+string DatosBiometricos::toString()
+{
+	stringstream ss;
+	ss << "--- Datos biometricos ---" << endl;
+	ss << "Estatura: " << estatura << endl;
+	ss << "Peso: " << peso << endl;
+	ss << "Porcentaje de grasa corporal: " << porcentajeGrasaCorporal << endl;
+	ss << "Porcentaje de masa muscular: " << porcentajeMasaMuscular << endl;
+	ss << "Fecha de medicion: " << fechaMedicion->toString() << endl;
+	return ss.str();
+}
---
+++ SistemaDeGestionParaClientes/DatosBiometricos.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Fecha.h"
+#include "DatosBiometricosInvalidosException.h"
+
+class DatosBiometricos
+{
+private:
+	float estatura;
+	float peso;
+	float porcentajeGrasaCorporal;
+	float porcentajeMasaMuscular;
+	Fecha* fechaMedicion;
+public:
+	DatosBiometricos(float estatura_ = 0, float peso_ = 0, float porcentajeGrasaCorporal_ = 0, float porcentajeMasaMuscular_ = 0, Fecha* fechaMedicion_ = new Fecha());
+	~DatosBiometricos();
+	//setters
+	void setEstatura(float estatura_);
+	void setPeso(float peso_);
+	void setPorcentajeGrasaCorporal(float porcentajeGrasaCorporal_);
+	void setPorcentajeMasaMuscular(float porcentajeMasaMuscular_);
+	void setFechaMedicion(Fecha* fechaMedicion_);
+	//getters
+	float getEstatura();
+	float getPeso();
+	float getPorcentajeGrasaCorporal();
+	float getPorcentajeMasaMuscular();
+	Fecha* getFechaMedicion();
+	//metodos
+	string toString();
+};
