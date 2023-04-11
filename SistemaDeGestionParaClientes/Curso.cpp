aba00679a59a9ed345e544c699ac428e8cfa23a1
charly <carlosgarita3110@gmail.com>
2023-04-10 18:26:55 -0600

    feat: Implement Curso class This commit adds the implementation of the Curso class. The Curso class has attributes such as descripcion, nivel, cupoMaximo, cantidadClientes, fechaInicio, fechaFin, horario, and listaClientesInscritos. It also has methods to set and get these attributes and to add or remove clients from the course. Additionally, it has a toString method that returns a string representation of the class. The implementation includes error handling for when the course is already full and the user tries to add a new client.

---
+++ SistemaDeGestionParaClientes/Curso.cpp
@@ -0,0 +1,131 @@
+#include "Curso.h"
+
+Curso::Curso(string descripcion_, string nivel_, int cupoMaximo_, Fecha* fechaInicio_, Fecha* fechaFin_, string horario_)
+    : descripcion(descripcion_), nivel(nivel_), cupoMaximo(cupoMaximo_), fechaInicio(fechaInicio_), fechaFin(fechaFin_), horario(horario_)
+{
+    this->cantidadClientes = 0;
+    this->listaClientesInscritos = new ListaEnlazada<Cliente>();
+}
+
+Curso::~Curso()
+{
+}
+
+void Curso::setDescripcion(string descripcion_)
+{
+    this->descripcion = descripcion_;
+}
+
+void Curso::setNivel(string nivel_)
+{
+    this->nivel = nivel_;
+}
+
+void Curso::setCupoMaximo(int cupoMaximo_)
+{
+    this->cupoMaximo = cupoMaximo_;
+}
+
+void Curso::setFechaInicio(Fecha* fechaInicio_)
+{
+    this->fechaInicio = fechaInicio_;
+}
+
+void Curso::setFechaFin(Fecha* fechaFin_)
+{
+    this->fechaFin = fechaFin_;
+}
+
+void Curso::setHorario(string horario_)
+{
+    this->horario = horario_;
+}
+
+const string Curso::getDescripcion() const
+{
+    return this->descripcion;
+}
+
+const string Curso::getNivel() const
+{
+    return this->nivel;
+}
+
+const int Curso::getCupoMaximo() const
+{
+    return this->cupoMaximo;
+}
+
+Fecha* Curso::getFechaInicio()
+{
+    return this->fechaInicio;
+}
+
+Fecha* Curso::getFechaFin()
+{
+    return this->fechaFin;
+}
+
+const string Curso::getHorario() const
+{
+    return this->horario;
+}
+
+const int Curso::getCantidadClientes() const
+{
+    return this->cantidadClientes;
+}
+
+ListaEnlazada<Cliente>* Curso::getListaClientesInscritos()
+{
+    return this->listaClientesInscritos;
+}
+
+void Curso::agregarCliente(Cliente* cliente_)
+{
+    //verificar que haya cupo disponible
+    if (this->tieneCupoDisponible())
+    {
+		listaClientesInscritos->insertar(cliente_);
+		this->cantidadClientes++;
+    }
+    else {
+        throw CupoMaximoExcedido("No hay cupo disponible para este curso.");
+    }
+}
+
+void Curso::eliminarCliente(Cliente* cliente_)
+{
+    listaClientesInscritos->eliminarDato(cliente_);
+}
+
+void Curso::mostrarClientes()
+{
+    ListaClienteUtils::MostrarClientes(listaClientesInscritos);
+}
+
+bool Curso::tieneCupoDisponible()
+{
+    return this->cantidadClientes < cupoMaximo;
+}
+
+const string Curso::toString() const
+{
+    stringstream ss;
+    ss << "Descripcion del curso: " << this->descripcion << endl
+        << "Nivel: " << this->nivel << endl
+        << "Cupo maximo: " << this->cupoMaximo << endl
+        << "Fecha inicio: " << this->fechaInicio->toString() << endl
+        << "Fecha fin: " << this->fechaFin->toString() << endl
+        << "Horario: " << this->horario << endl
+        << "Cantidad de clientes inscritos: " << this->cantidadClientes << endl
+        << "Lista de clientes inscritos: " << endl
+        << ListaClienteUtils::MostrarClientes(this->listaClientesInscritos);
+    return ss.str();
+}
+
+void Curso::agregarListaClientes(ListaEnlazada<Cliente>* listaClientes_)
+{
+    this->listaClientesInscritos = listaClientes_;
+    this->cantidadClientes = listaClientes_->getCantidad();
+}
---
+++ SistemaDeGestionParaClientes/Curso.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "ListaClienteUtils.h"	// esto incluye a ListaEnlazada.h
+#include "Fecha.h"
+#include "CupoMaximoExcedido.h"
+
+class Curso
+{
+private:
+	string descripcion;
+	string nivel; //principiante, intermedio, avanzado
+	int cupoMaximo;
+	int cantidadClientes;
+	//fechas y horario del curso
+	Fecha* fechaInicio;
+	Fecha* fechaFin;
+	string horario;
+	//lista de clientes inscriptos
+	ListaEnlazada<Cliente>* listaClientesInscritos;	
+public:
+	Curso(string descripcion_ = "", string nivel_ = "", int cupoMaximo_ = 0, Fecha* fechaInicio_ = new Fecha(), Fecha* fechaFin_ = new Fecha(), string horario_ = "");
+	~Curso();
+	// setters
+	void setDescripcion(string descripcion_);
+	void setNivel(string nivel_);
+	void setCupoMaximo(int cupoMaximo_);
+	void setFechaInicio(Fecha* fechaInicio_);
+	void setFechaFin(Fecha* fechaFin_);
+	void setHorario(string horario_);
+	// getters
+	const string getDescripcion() const;
+	const string getNivel() const;
+	const int getCupoMaximo() const;
+	Fecha* getFechaInicio();
+	Fecha* getFechaFin();
+	const string getHorario() const;
+	const int getCantidadClientes() const;	// cantidad de clientes inscritos en el curso
+	ListaEnlazada<Cliente>* getListaClientesInscritos();
+	// metodos
+	void agregarCliente(Cliente* cliente);	//agregar reserva
+	void eliminarCliente(Cliente* cliente);	//eliminar reserva
+	void mostrarClientes();	//mostrar reservas
+	bool tieneCupoDisponible();
+	const string toString() const;
+	//metodo experimental para agregar una lista directamente. | es un set de listaClientesInscritos
+	void agregarListaClientes(ListaEnlazada<Cliente>* listaClientes_);
+};
