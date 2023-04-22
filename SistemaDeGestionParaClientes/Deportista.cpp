4bc409e6b35451e0ec06522c1575423ff1633770
charly <carlosgarita3110@gmail.com>
2023-04-22 11:58:22 -0600

    feat(deportista): Make Deportista class abstract

--- SistemaDeGestionParaClientes/Deportista.cpp
+++ SistemaDeGestionParaClientes/Deportista.cpp
@@ -1,58 +1,22 @@
 #include "Deportista.h"
 
-Deportista::Deportista(const Deportista& deportista_)
-{
-    try
-    {
-        this->fechaNacimiento = new Fecha(*deportista_.fechaNacimiento);
-    }
-    catch (FechaInvalidaException& e)
-    {
-        throw DeportistaInvalidoException("Fecha de nacimiento invalida");
-    }
-    try
-    {
-        this->datosBiometricos = new DatosBiometricos(*deportista_.datosBiometricos);
-    }
-    catch (DatosBiometricosInvalidosException& e)
-    {
-        throw DeportistaInvalidoException("Datos biometricos invalidos");
-    }
-    this->cedula = deportista_.cedula;
-    this->nombre = deportista_.nombre;
-    this->telefono = deportista_.telefono;
-    this->sexo = deportista_.sexo;
-    this->estado = deportista_.estado;
-}
 
-Deportista::Deportista(string cedula_, string nombre_, string telefono_, Fecha* fechaNacimiento_, string sexo_, char estado, DatosBiometricos* datosBiometricos_)
+Deportista::Deportista(string cedula_, string nombre_, string telefono_, Fecha* fechaNacimiento_, char estado_):
+    cedula(cedula_),
+	nombre(nombre_),
+	telefono(telefono_),
+	estado(estado_)
 {
-    try
-    {
+    try {
         this->fechaNacimiento = new Fecha(*fechaNacimiento_);
-    }
-    catch (FechaInvalidaException& e)
+	}
+    catch (FechaInvalidaException& e) 
     {
-        throw DeportistaInvalidoException("Fecha de nacimiento invalida");
+		throw DeportistaInvalidoException("Fecha de nacimiento invalida");
     }
-
-    try
-    {
-        this->datosBiometricos = new DatosBiometricos(*datosBiometricos_);
-    }
-    catch (DatosBiometricosInvalidosException& e)
-    {
-        throw DeportistaInvalidoException("Datos biometricos invalidos");
-    }
-
-    this->cedula = cedula_;
-    this->nombre = nombre_;
-    this->telefono = telefono_;
-    this->sexo = sexo_;
-    this->estado = estado;
 }
 
-Deportista::~Deportista()
+Deportista::Deportista(const Deportista& deportista_)
 {
 }
 
@@ -83,23 +47,6 @@ void Deportista::setFechaNacimiento(Fecha* fechaNacimiento_)
     }
 }
 
-void Deportista::setSexo(string sexo_)
-{
-    this->sexo = sexo_;
-}
-
-void Deportista::setDatosBiometricos(DatosBiometricos* datosBiometricos_)
-{
-    try
-    {
-        this->datosBiometricos = new DatosBiometricos(*datosBiometricos_);
-    }
-    catch (DatosBiometricosInvalidosException& e)
-    {
-        throw DeportistaInvalidoException("Datos biometricos invalidos");
-    }
-}
-
 void Deportista::setEstado(char estado)
 {
     this->estado = estado;
@@ -125,58 +72,25 @@ Fecha* Deportista::getFechaNacimiento()
     return this->fechaNacimiento;
 }
 
-const string Deportista::getSexo() const
-{
-    return this->sexo;
-}
-
-DatosBiometricos* Deportista::getDatosBiometricos()
-{
-    return this->datosBiometricos;
-}
-
 const char Deportista::getEstado() const
 {
     return this->estado;
 }
 
-const string Deportista::toString() const
-{
-    stringstream ss;
-    ss << "Cedula: " << this->cedula << endl;
-    ss << "Nombre: " << this->nombre << endl;    ss << "Telefono: " << this->telefono << endl;
-    ss << "Fecha de nacimiento: " << this->fechaNacimiento->toString() << endl;
-    ss << "Sexo: " << this->sexo << endl;
-    ss << "Estado: " << this->estado << endl;
-    ss << this->datosBiometricos->toString() << endl;
-    return ss.str();
-}
-
 Deportista& Deportista::operator=(const Deportista& deportista_)
 {
-    try
+    if (this != &deportista_)
     {
-        this->fechaNacimiento = new Fecha(*deportista_.fechaNacimiento);
-    }
-    catch (FechaInvalidaException& e)
-    {
-        throw DeportistaInvalidoException("Fecha de nacimiento invalida");
-    }
-    try
-    {
-        this->datosBiometricos = new DatosBiometricos(*deportista_.datosBiometricos);
-    }
-    catch (DatosBiometricosInvalidosException& e)
-    {
-        throw DeportistaInvalidoException("Datos biometricos invalidos");
-    }
-    this->cedula = deportista_.cedula;
-    this->nombre = deportista_.nombre;
-    this->telefono = deportista_.telefono;
-    this->sexo = deportista_.sexo;
-    this->estado = deportista_.estado;
+		this->cedula = deportista_.cedula;
+		this->nombre = deportista_.nombre;
+		this->telefono = deportista_.telefono;
+		this->fechaNacimiento = deportista_.fechaNacimiento;
+		this->estado = deportista_.estado;
+	}
     return *this;
 }
+
+
 
 ostream& operator<<(ostream& out, Deportista& deportista_)
 {
