c822a94cf937c9f942f2ffc89a87d211de31447a
charly <carlosgarita3110@gmail.com>
2023-04-26 15:58:07 -0600

    feat(Pago): add Pago class

---
+++ SistemaDeGestionParaClientes/Pago.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Fecha.h"
+
+class Pago
+{
+private:
+	Fecha* fecha;
+	string mesCancelado;
+	double monto;
+public:
+	Pago(Fecha* fecha_, string mesCancelado_, double monto_);
+	Pago(const Pago& pago_);
+	virtual ~Pago();
+	// setters
+	void setFecha(Fecha* fecha_);
+	void setMesCancelado(string mesCancelado_);
+	void setMonto(double monto_);
+	// getters
+	Fecha* getFecha();
+	const string getMesCancelado() const;
+	const double getMonto() const;
+	// metodos
+	const string toString() const;
+	// sobrecarga de operadores
+	Pago& operator=(const Pago& pago_);
+	friend ostream& operator<<(ostream& out, const Pago& pago_);
+};
